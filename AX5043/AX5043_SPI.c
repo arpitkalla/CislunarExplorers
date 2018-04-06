2b809caf56aeab42c8c727309ca600b32f3eeb50
arpit-kalla <ak2242@cornell.edu>
2018-04-05 20:28:17 -0400

    Fixing Wrapper

--- AX5043/AX5043_SPI.c
+++ AX5043/AX5043_SPI.c
@@ -12,28 +12,25 @@
 static char module_docstring[] =
     "This module provides an interface for AX5043 Antenna.";
 
-static char writePacket_docstring[] =
-    "Write packets to send.";
+static char writeReg_docstring[] =
+    "Write packets to a register.";
 
-static PyObject *ax5043_writeReg(PyObject *self, PyObject *args);
 
 static PyMethodDef module_methods[] = {
-    {"writeReg", ax5043_writeReg, METH_VARARGS, writePacket_docstring},
+    {"writeReg", ax5043_ax5043_writeReg, METH_VARARGS, writePacket_docstring},
     {NULL, NULL, 0, NULL}
 };
 
-
-
-PyMODINIT_FUNC init_AX5043(void)
+PyMODINIT_FUNC init_ax5043(void)
 {
     PyObject *m = Py_InitModule3("_AX5043", module_methods, module_docstring);
     if (m == NULL)
         return;
 }
 
-static PyObject *ax5043_writeReg(PyObject *self, PyObject *args){
+static PyObject *ax_5043_ax5043_writeReg(PyObject *self, PyObject *args){
     uint16_t addr, unsigned char value;
-     if (!PyArg_ParseTuple(args, "IB", &addr, &value))
+     if (!PyArg_ParseTuple(args, "IB", addr, value))
         return NULL;
     ax5043_writeReg(addr,value);
 }
