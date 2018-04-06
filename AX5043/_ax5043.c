d35a40196ffb36db2adc878bd6d8110a06a6e869
arpit-kalla <ak2242@cornell.edu>
2018-04-05 21:04:19 -0400

    Fixing Wrapper

--- AX5043/_ax5043.c
+++ AX5043/_ax5043.c
@@ -10,25 +10,14 @@ static char module_docstring[] =
 static char ax5043_writeReg_docstring[] =
     "Write packets to a register.";
 
-static PyObject *ax5043_writeReg(PyObject *self, PyObject *args);
-
-static PyObject *ax5043_writeReg(PyObject *self, PyObject *args){
-    uint16_t addr;
-    unsigned char value;
-     if (!PyArg_ParseTuple(args, "IB", &addr, &value))
-        return NULL;
-    ax5043_writeReg(addr,value);
+static PyObject *ax5043_ax5043_writeReg(PyObject *self, PyObject *args);
 
 
-
-    PyObject *ret = Py_BuildValue("B", value);
-    return ret;
-}
 
 
 
 static PyMethodDef module_methods[] = {
-    {"writeReg", ax5043_writeReg, METH_VARARGS, ax5043_writeReg_docstring},
+    {"writeReg", ax_5043_ax5043_writeReg, METH_VARARGS, ax5043_writeReg_docstring},
     {NULL, NULL, 0, NULL}
 };
 
@@ -50,9 +39,19 @@ PyMODINIT_FUNC PyInit__ax5043(void)
     module = PyModule_Create(&moduledef);
     if (!module) return NULL;
 
-    /* Load `numpy` functionality. */
-    import_array();
-
     return module;
+}
+
+static PyObject *ax5043_ax5043_writeReg(PyObject *self, PyObject *args){
+    uint16_t addr;
+    unsigned char value;
+     if (!PyArg_ParseTuple(args, "IB", &addr, &value))
+        return NULL;
+    ax5043_writeReg(addr,value);
+
+
+
+    PyObject *ret = Py_BuildValue("B", value);
+    return ret;
 }
 
