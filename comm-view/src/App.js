7c99208deef6be40b37c39a30f10d79b6a7d1ae1
arpit-kalla <ak2242@cornell.edu>
2018-04-13 20:38:52 -0400

    Adding Command Form to Frontend

--- comm-view/src/App.js
+++ comm-view/src/App.js
@@ -3,16 +3,53 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      commands: [],
+      isLoading: false
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ isLoading: true });
+
+  fetch("http://localhost:5000/commands"
+   )
+      .then(response => response.json())
+      .then(commands => this.setState({ commands: commands.commands, isLoading: false }));
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
   render() {
+    const { commands, isLoading } = this.state;
+    if(isLoading){
+      return <p>Loading...</p>
+    }
+    console.log(commands)
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Cislunar Explorers Ground Station</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Press submit to send commands to the Satellite.
         </p>
+        
+        <form id = "form_commands" onSubmit={this.handleSubmit}>
+         {commands.map(function(listValue){
+            return (<div> {listValue} :  <input type="text"></input> <br /><br /></div>)
+          })}
+
+         <input type="submit" value="Submit" />
+        </form>
+
       </div>
     );
   }
