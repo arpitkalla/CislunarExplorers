409d823b670047678b4bfc64431e789e48e3d33e
arpit-kalla <ak2242@cornell.edu>
2018-04-20 16:18:45 -0400

    Adding command to the wrapper

--- AX5043/_ax5043.c
+++ AX5043/_ax5043.c
@@ -6,6 +6,8 @@
 static char module_docstring[] =
     "This module provides an interface for AX5043 Antenna.";
 
+static char ax5043_init_docstring[] = 
+    "Inititalize the antenna.";
 static char ax5043_write_docstring[] = 
     "Write packets to AX_REG_FIFODATA register.";
 static char ax5043_write_reg_docstring[] = 
@@ -17,6 +19,7 @@ static char ax5043_set_reg_tx_docstring[] =
 static char ax5043_set_reg_rx_docstring[] = 
     "Setting up AX5043 to RX mode.";
 
+static PyObject *ax5043_ax5043_init(PyObject *self, PyObject *args);
 static PyObject *ax5043_ax5043_write(PyObject *self, PyObject *args);
 static PyObject *ax5043_ax5043_write_reg(PyObject *self, PyObject *args);
 static PyObject *ax5043_ax5043_read_reg(PyObject *self, PyObject *args);
@@ -24,6 +27,7 @@ static PyObject *ax5043_ax5043_set_reg_tx(PyObject *self, PyObject *args);
 static PyObject *ax5043_ax5043_set_reg_rx(PyObject *self, PyObject *args);
 
 static PyMethodDef module_methods[] = {
+    {"init", ax5043_ax5043_init, METH_VARARGS, ax5043_init_docstring},
     {"write", ax5043_ax5043_write, METH_VARARGS, ax5043_write_docstring},
     {"write_reg", ax5043_ax5043_write_reg, METH_VARARGS, ax5043_write_reg_docstring},
     {"read_reg", ax5043_ax5043_read_reg, METH_VARARGS, ax5043_read_reg_docstring},
@@ -51,6 +55,10 @@ PyMODINIT_FUNC PyInit__ax5043(void)
     if (!module) return NULL;
 
     return module;
+}
+
+static PyObject *ax5043_ax5043_write(PyObject *self, PyObject *args){
+    ax5043_init();
 }
 
 static PyObject *ax5043_ax5043_write(PyObject *self, PyObject *args){
